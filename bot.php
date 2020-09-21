f0d00c43f22058743fa12c3f551ace3ebcaa9624
Simone Cosimo <simosx4@gmail.com>
2020-09-21 14:14:12 +0200

    @admin(groups) reports message in private to admin

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ class MyEventHandler extends EventHandler
     /**
      * @var int|string Username or ID of bot admin
      */
-    const ADMIN = "simocosimo"; 
+    const ADMIN = "@simocosimo"; 
     /**
      * Get peer(s) where to report errors
      *
@@ -46,7 +46,7 @@ class MyEventHandler extends EventHandler
      */
     public function onUpdateNewChannelMessage(array $update): \Generator
     {
-        return $this->onUpdateNewMessage($update);
+        return onUpdateNewMessage($update);
     }
     /**
      * Handle updates from users.
@@ -61,27 +61,64 @@ class MyEventHandler extends EventHandler
             $update['message']['out'] ?? false) {
                 return;
         }
-        $res = \json_encode($update, JSON_PRETTY_PRINT);
+        
+        // If it's a user chat
+        if($update['message']['to_id']['_'] !== 'peerChat') {
+            $res = \json_encode($update, JSON_PRETTY_PRINT);
 
-        try {
-            yield $this->messages->sendMessage([
-                'peer' => $update, 
-                'message' => "<code>$res</code>", 
-                'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 
-                'parse_mode' => 'HTML']);
+            try {
+                yield $this->messages->sendMessage([
+                    'peer' => $update, 
+                    'message' => "<code>$res</code>", 
+                    'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 
+                    'parse_mode' => 'HTML']);
 
-            if (isset($update['message']['media']) && 
-                $update['message']['media']['_'] !== 'messageMediaGame') {
-                    yield $this->messages->sendMedia([
-                        'peer' => $update, 
-                        'message' => $update['message']['message'], 
-                        'media' => $update]);
-            }
-        } catch (RPCErrorException $e) {
-            $this->report("Surfaced: $e");
-        } catch (Exception $e) {
-            if (\stripos($e->getMessage(), 'invalid constructor given') === false) {
+                if (isset($update['message']['media']) && 
+                    $update['message']['media']['_'] !== 'messageMediaGame') {
+                        yield $this->messages->sendMedia([
+                            'peer' => $update, 
+                            'message' => $update['message']['message'], 
+                            'media' => $update]);
+                }
+            } catch (RPCErrorException $e) {
                 $this->report("Surfaced: $e");
+            } catch (Exception $e) {
+                if (\stripos($e->getMessage(), 'invalid constructor given') === false) {
+                    $this->report("Surfaced: $e");
+                }
+            }
+        } else {
+            // if it's a group chat
+            try {
+                // $groupId = $update['update'];
+                $groupInfos = yield $this->getPwrChat($update);
+                $groupTitle = $groupInfos['title'];
+                $reportMessage = $update['message']['message'];
+                $reportSender = yield $this->users->getFullUser([
+                    'id' => $update['message']['from_id']
+                ]);
+                $senderUsername = $reportSender['user']['username'];
+                //$res = \json_encode($reportSender, JSON_PRETTY_PRINT);
+    
+                if(strpos($update['message']['message'], "@admin") !== FALSE) {
+                    yield $this->messages->sendMessage([
+                        'peer' => "@simocosimo", 
+                        'message' => "Segnalazione dal gruppo <b>$groupTitle</b> da parte di @$senderUsername:<br><code>$reportMessage</code>", 
+                        // 'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 
+                        'parse_mode' => 'HTML']); 
+
+                    yield $this->messages->sendMessage([
+                        'peer' => $update, 
+                        'message' => "Segnalazione inviata agli admin!", 
+                        'reply_to_msg_id' => isset($update['message']['id']) ? $update['message']['id'] : null, 
+                        'parse_mode' => 'HTML']); 
+                } 
+            } catch (RPCErrorException $e) {
+                $this->report("Surfaced: $e");
+            } catch (Exception $e) {
+                if (\stripos($e->getMessage(), 'invalid constructor given') === false) {
+                    $this->report("Surfaced: $e");
+                }
             }
         }
     }
